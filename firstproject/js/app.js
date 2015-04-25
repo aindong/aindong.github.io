7590f142bbdd354a31dfb855e6d90926080b134a
Alleo Indong <freedom_war550@live.com.ph>
2015-04-26 03:22:08 +0800

    Detecting a city

--- firstproject/js/app.js
+++ firstproject/js/app.js
@@ -6,10 +6,27 @@
 var app = angular.module('app', []);
 
 app.controller('MainController', ['$scope', '$http', function ($scope, $http) {
-    var promise = $http.get('http://api.openweathermap.org/data/2.5/forecast/daily?id=1690039&mode=json&units=metric&cnt=7');
 
-    promise.then(function(res) {
-        //console.log(res.data);
-        $scope.forecasts = res.data;
-    });
+    function getLocation() {
+        navigator.geolocation.getCurrentPosition(foundLocation, noLocation);
+
+        function foundLocation(position) {
+            var lat = position.coords.latitude;
+            var long = position.coords.longitude;
+            console.log('lat ' + lat + ' long ' + long);
+            var promise = $http.get('http://api.openweathermap.org/data/2.5/forecast/daily?id=1682812&mode=json&units=metric&cnt=7');
+
+            promise.then(function(res) {
+                console.log(res.data);
+                $scope.forecasts = res.data;
+            });
+        }
+
+        function noLocation() {
+            alert('Sorry I cant determine your location');
+        }
+    }
+
+    getLocation();
+
 }]);
